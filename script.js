87d0708e297b7f9f0ccc3c895fe08cf52f5ad768
zmissipsa <zouaouimissipsa@gmail.com>
2022-11-23 20:36:03 +0100

    mod

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 (function (window) {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
-    for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0).toLowerCase();
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
 
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
     }
-    
-    })(window);
+  }
+})(window);
